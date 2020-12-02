d231561eff3913152c18b085c472be6013a61142
Xhy123456 <492087919@qq.com>
2020-12-02 21:12:29 +0800

    老师页面小改动

--- src/components/Teacher/search_group.vue
+++ src/components/Teacher/search_group.vue
@@ -1,35 +1,63 @@
 <template>
   <div>
-    <el-breadcrumb separator-class="el-icon-arrow-right">
+    <el-breadcrumb separator-class="el-icon-arrow-right" >
       <el-breadcrumb-item :to="{ path: '/welcome' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>信息查询</el-breadcrumb-item>
       <el-breadcrumb-item>我的小组</el-breadcrumb-item>
     </el-breadcrumb>
 
     <div class="a">
+      <h2>小组1</h2>
         <el-table
-            :data="tableData"
-            style="width: 90%">
+            :data="tableData1"
+            style="width: 90%"
+            :row-class-name="tableRowClassName">
           <el-table-column
-              prop="group_num"
-              label="组长学号"
-              width="180">
-          </el-table-column>
-          <el-table-column
-              prop="group_leader_name"
-              label="组长"
-              width="180">
-          </el-table-column>
-          <el-table-column
-              prop="number"
+              prop="id"
               label="学号"
               width="180">
           </el-table-column>
           <el-table-column
               prop="name"
-              label="姓名">
+              label="姓名"
+              width="180">
+          </el-table-column>
+          <el-table-column
+              prop="phone_num"
+              label="电话"
+              width="180">
+          </el-table-column>
+          <el-table-column
+              prop="email"
+              label="邮箱">
           </el-table-column>
         </el-table>
+      <br>
+      <h2>小组2</h2>
+      <el-table
+          :data="tableData2"
+          style="width: 90%"
+          :row-class-name="tableRowClassName">
+        <el-table-column
+            prop="id"
+            label="学号"
+            width="180">
+        </el-table-column>
+        <el-table-column
+            prop="name"
+            label="姓名"
+            width="180">
+        </el-table-column>
+        <el-table-column
+            prop="phone_num"
+            label="电话"
+            width="180">
+        </el-table-column>
+        <el-table-column
+            prop="email"
+            label="邮箱">
+        </el-table-column>
+      </el-table>
       </div>
     </div>
 </template>
@@ -39,15 +67,64 @@
 
 <script>
 export default {
+  methods: {
+    tableRowClassName({rowIndex}) {
+      if (rowIndex === 0) {
+        return 'success-row';
+      }
+    }
+  },
   data() {
     return {
-      tableData: [{
-        group_num: '41811002',
-        group_leader_name: '王元浩',
-        number: '41811001',
-        name:'向宏宇',
+      tableData1: [{
+        id: '41811002',
+        name: '王元浩',
+        phone_num: '10086',
+        email:'123456@qq.com',
+      },{
+        id: '41811002',
+        name: '王元浩',
+        phone_num: '10086',
+        email:'123456@qq.com',
+      },{
+        id: '41811002',
+        name: '王元浩',
+        phone_num: '10086',
+        email:'123456@qq.com',
+      },{
+        id: '41811002',
+        name: '王元浩',
+        phone_num: '10086',
+        email:'123456@qq.com',
+      },],
+      tableData2: [{
+        id: '41811002',
+        name: '王元浩',
+        phone_num: '10086',
+        email:'123456@qq.com',
+      },{
+        id: '41811002',
+        name: '王元浩',
+        phone_num: '10086',
+        email:'123456@qq.com',
+      },{
+        id: '41811002',
+        name: '王元浩',
+        phone_num: '10086',
+        email:'123456@qq.com',
+      },{
+        id: '41811002',
+        name: '王元浩',
+        phone_num: '10086',
+        email:'123456@qq.com',
       },]
     }
   }
 }
 </script>
+
+<style>
+.el-table .success-row {
+  background: #f0f9eb;
+}
+</style>
